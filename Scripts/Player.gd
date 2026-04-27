3d70d578aa0c164372ff683dcd865961d85cacc9
uzakotim <timauzakov@gmail.com>
2026-04-27 14:11:22 +0500

    bigger robot

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,17 +1,17 @@
 extends CharacterBody2D
 
 @export_group("Movement")
-@export var speed = 300.0
-@export var acceleration = 800.0
-@export var friction = 600.0
+@export var speed = 800.0
+@export var acceleration = 2000.0
+@export var friction = 1500.0
 
 @export_group("Physics")
-@export var jump_velocity = -400.0
-@export var gravity = 980.0
+@export var jump_velocity = -1000.0
+@export var gravity = 2500.0
 
 @export_group("Feel")
-@export var coyote_time = 0.15 # Seconds to allow jump after leaving ledge
-@export var jump_buffer_time = 0.1 # Seconds to "remember" jump press before landing
+@export var coyote_time = 0.15
+@export var jump_buffer_time = 0.1
 
 var coyote_timer = 0.0
 var jump_buffer_timer = 0.0
@@ -25,17 +25,17 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	else:
-		coyote_timer = coyote_time # Reset coyote time while on floor
+		coyote_timer = coyote_time
 
 	# Handle Jump Input & Buffering
 	if Input.is_action_just_pressed("ui_accept"):
 		jump_buffer_timer = jump_buffer_time
 
-	# Execute Jump (Coyote Time + Buffering)
+	# Execute Jump
 	if jump_buffer_timer > 0 and coyote_timer > 0:
 		velocity.y = jump_velocity
-		jump_buffer_timer = 0.0 # Clear buffer
-		coyote_timer = 0.0 # Clear coyote
+		jump_buffer_timer = 0.0
+		coyote_timer = 0.0
 
 	# Get Horizontal Input
 	var direction = Input.get_axis("ui_left", "ui_right")
