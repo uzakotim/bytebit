eb047d55d877c223f0462b6e2282008149116b34
uzakotim <timauzakov@gmail.com>
2026-04-27 13:56:30 +0500

    following camera

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,24 +9,40 @@ extends CharacterBody2D
 @export var jump_velocity = -400.0
 @export var gravity = 980.0
 
+@export_group("Feel")
+@export var coyote_time = 0.15 # Seconds to allow jump after leaving ledge
+@export var jump_buffer_time = 0.1 # Seconds to "remember" jump press before landing
+
+var coyote_timer = 0.0
+var jump_buffer_timer = 0.0
+
 func _physics_process(delta):
+	# Handle Timers
+	coyote_timer -= delta
+	jump_buffer_timer -= delta
+
 	# Apply Gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		coyote_timer = coyote_time # Reset coyote time while on floor
 
-	# Handle Jump
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# Handle Jump Input & Buffering
+	if Input.is_action_just_pressed("ui_accept"):
+		jump_buffer_timer = jump_buffer_time
+
+	# Execute Jump (Coyote Time + Buffering)
+	if jump_buffer_timer > 0 and coyote_timer > 0:
 		velocity.y = jump_velocity
+		jump_buffer_timer = 0.0 # Clear buffer
+		coyote_timer = 0.0 # Clear coyote
 
-	# Get Horizontal Input (-1, 0, 1)
+	# Get Horizontal Input
 	var direction = Input.get_axis("ui_left", "ui_right")
 	
-	# Skid-Drive Logic (Acceleration & Friction)
 	if direction != 0:
-		# Gradually reach target speed
 		velocity.x = move_toward(velocity.x, direction * speed, acceleration * delta)
 	else:
-		# Gradually come to a stop
 		velocity.x = move_toward(velocity.x, 0, friction * delta)
 
 	move_and_slide()
